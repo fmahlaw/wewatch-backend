412ec6e628a0b2c9c8c94cf13ad9dc6267777ef0
fmahlaw <ferianmahlaw@gmail.com>
2023-02-18 18:18:45 +0700

    added home

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { app,PORT } from "./app.js";
 import storeRoutes from "./routes/stores.js";
 
+app.get("/", (req,res)=>res.send("This is Wewatch API working fine"))
 app.use("/post_name_toped", storeRoutes);
 
 app.listen(PORT, () => {
